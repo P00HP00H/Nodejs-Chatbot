a296241cec12f3b8be110a8a347fb72daf373946
P00HP00H <32670787+P00HP00H@users.noreply.github.com>
2019-09-01 03:30:41 +0900

    실행파일(node app.js)

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ var express = require('express');
 var http = require('http');
 var app = express();
 var bodyParser = require('body-parser');
-//var db_Router = require('./router/db');
+var db_fetch_Router = require('./router/db_fetch');
 var keyboard_Router = require('./router/keyboard');
 var message_Router = require('./router/message');
+
+db_fetch_Router.fetch();    // ./router/db.fetch.js 실행
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-
 app.use('/keyboard', keyboard_Router);
 app.use('/message', message_Router);
 
