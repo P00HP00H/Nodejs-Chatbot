364d0aef9ceea201938e4088acbdf35072af1268
P00HP00H <32670787+P00HP00H@users.noreply.github.com>
2019-09-08 06:08:35 +0900

    node.js 컨테이너 실행 및 해당 파일들을 node.js 컨테이너로 복사

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 chmod 764 node.sh
 chmod 764 start-dev.sh
 docker-compose up -d
-docker cp node.sh nodejschatbot_node_1:/home
-docker cp app.js nodejschatbot_node_1:/home
-docker cp router/ nodejschatbot_node_1:/home/
+docker cp node.sh nodejschatbot_node_1:/
+docker cp app.js nodejschatbot_node_1:/
+docker cp router/ nodejschatbot_node_1:/
 docker cp start-dev.sh nodejschatbot_node_1:/
-docker exec nodejschatbot_node_1 /bin/bash /start-dev.sh
+sleep 3s
+docker exec nodejschatbot_node_1 /bin/bash ./start-dev.sh
