26bd44e08b0a0adb07ef7890b43d69822d39f068
P00HP00H <32670787+P00HP00H@users.noreply.github.com>
2019-09-08 03:52:36 +0900

    nodejs 컨테이너 환경 세팅

--- node.sh
+++ node.sh
@@ -1,10 +1,4 @@
 apt-get update -y
-apt-get install vim -y
-apt-get install net-tools -y
-apt-get install ssh -y
-npm install mysql -y
-npm install ejs --save
-npm install express-mysql-session --save
 npm install express --save
 npm install --save express-session
 npm install body-parser --save
