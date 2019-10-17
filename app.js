a5407e9e235f650fc92ad2aba3d12ef4d4ce83d4
P00HP00H <32670787+P00HP00H@users.noreply.github.com>
2019-10-18 05:32:34 +0900

    실행파일(node app.js)

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var http = require('http');
 var app = express();
 var bodyParser = require('body-parser');
 var db_create_Router = require('./router/db_create');
-var db_fetch_Router = require('./router/db_fetch');
+var db_insert_Router = require('./router/db_insert');
 var db_delete_Router = require('./router/db_delete');
 var keyboard_Router = require('./router/keyboard');
 var message_Router = require('./router/message');
@@ -12,15 +12,15 @@ var cron = require('node-cron');
 cron.schedule('0 0 0 * * *', function(){    // 매일 자정에 db 초기화 -> 버스 시간표가 바뀔 수 있으므로 매일 업데이트해주는 개념
     console.log('crontab on!!');
     db_delete_Router.db_delete();   // db 삭제 -> 정확히는 각 collection들을 삭제 -> ./router/db_delete.js의 db_delete()를 실행
-    setTimeout(function() {     // db_delete()이 끝나기도 전에 fetch()가 실행되는 것을 방지
-        db_fetch_Router.fetch();    // ./router/db.fetch.js 실행
+    setTimeout(function() {     // db_delete()이 끝나기도 전에 insert()가 실행되는 것을 방지
+        db_insert_Router.insert();    // ./router/db.insert.js 실행
     }, 1000)
 });
 
 db_create_Router.db_create();       // ./router/db.create.js 실행
 
 setTimeout(function() {
-    db_fetch_Router.fetch();    // ./router/db.fetch.js 실행
+    db_insert_Router.insert();    // ./router/db.insert.js 실행
 }, 1000)
 
 app.use(bodyParser.urlencoded({extended: false}));
