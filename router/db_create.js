c4ec9002b89b8bc3e6079ce2cef3907ca361f9d1
P00HP00H <32670787+P00HP00H@users.noreply.github.com>
2019-10-18 06:13:04 +0900

    DB 및 Collection 생성

---
+++ router/db_create.js
@@ -0,0 +1,34 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://db:27017/';
+var mongoose = require('mongoose');
+mongoose.connect(url);
+
+var bus_time = {};
+
+bus_time.db_create = function() {
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var bus_db = db.db("bus");
+        bus_db.createCollection("university_weekday", {capped: true, size: 2000, max: 2000}, function (err, res) {
+            if (err) throw err;
+            console.log("db.collection(university_weekday) created!");
+            db.close();
+        });
+        bus_db.createCollection("university_weekend", {capped: true, size: 2000, max: 2000}, function (err, res) {
+            if (err) throw err;
+            console.log("db.collection(university_weekend) created!");
+            db.close();
+        });
+        bus_db.createCollection("station_weekday", {capped: true, size: 2000, max: 2000}, function (err, res) {
+            if (err) throw err;
+            console.log("db.collection(station_weekday) created!");
+            db.close();
+        });
+        bus_db.createCollection("station_weekend", {capped: true, size: 2000, max: 2000}, function (err, res) {
+            if (err) throw err;
+            console.log("db.collection(station_weekend) created!");
+            db.close();
+        });
+    });
+}
+module.exports = bus_time;
