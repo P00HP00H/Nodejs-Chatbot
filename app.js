be299bc1ba1fac849c7741b1a698d3e58ce392b3
P00HP00H <32670787+P00HP00H@users.noreply.github.com>
2019-10-18 06:09:42 +0900

    실행파일(node app.js)

--- app.js
+++ app.js
@@ -12,12 +12,15 @@ var cron = require('node-cron');
 cron.schedule('0 0 0 * * *', function(){    // 매일 자정에 db 초기화 -> 버스 시간표가 바뀔 수 있으므로 매일 업데이트해주는 개념
     console.log('crontab on!!');
     db_delete_Router.db_delete();   // db 삭제 -> 정확히는 각 collection들을 삭제 -> ./router/db_delete.js의 db_delete()를 실행
-    setTimeout(function() {     // db_delete()이 끝나기도 전에 insert()가 실행되는 것을 방지
+    
+    /* 여기는 db_create가 따로 없는데 사실은 굳이 db_create를 해주지 않고 바로 db_insert를 해주면 db와 collection이 생성된다. */
+    
+    setTimeout(function() {     // db_delete()이 끝나기도 전에 insert()가 실행되는 것을 방지하기 위해 setTimeout함수를 이용
         db_insert_Router.insert();    // ./router/db.insert.js 실행
     }, 1000)
 });
 
-db_create_Router.db_create();       // ./router/db.create.js 실행
+db_create_Router.db_create();   // ./router/db.create.js 실행 -> 사실 create를 안해도 되지만 db를 설정에 맞게 create할 수 있다는 것을 보이기 위해 넣었음
 
 setTimeout(function() {
     db_insert_Router.insert();    // ./router/db.insert.js 실행
